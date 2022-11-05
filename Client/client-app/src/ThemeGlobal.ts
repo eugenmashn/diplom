24be86e94ea6b5fe037589b4b4ebe8a0e9818859
Mashniskuy Evhene <mashniskuy@gmail.com>
2022-11-05 19:06:37 +0200

    change color theme

--- Client/client-app/src/ThemeGlobal.ts
+++ Client/client-app/src/ThemeGlobal.ts
@@ -3,6 +3,12 @@
 import createTheme from '@mui/material/styles/createTheme';
 
 export const themeGlobal = createTheme({
+    palette: {
+        mode: 'dark',
+        // primary: {
+        //     main: '#1976d2',
+        // },
+    },
     components: {
 
     },
